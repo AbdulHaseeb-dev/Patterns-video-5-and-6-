5d0e17e27a41b979f7774bc53a053954cbeae20d
AbdulHaseeb-dev <haseebrana.faisal@gmail.com>
2024-02-21 14:48:45 +0500

    hollow rectangle

--- HollowRectangle.java
+++ HollowRectangle.java
@@ -1,8 +1,7 @@
 import java.util.Scanner;
 
-public class solid_rectangle {
+public class HollowRectangle {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter n");
         int n = sc.nextInt();
@@ -11,10 +10,14 @@ public class solid_rectangle {
 
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
-                System.out.print("*");
+                if (i == 1 || j == 1 || i == n || j == m) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
             }
             System.out.println();
         }
-       sc.close();
+        sc.close();
     }
 }
